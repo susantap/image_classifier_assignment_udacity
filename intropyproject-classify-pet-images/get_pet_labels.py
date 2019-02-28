8cff0c731c50d3fa5fd18d0d211c5eec6905ba43
susanta-vi <susanta.pattanayak@virtualinstruments.com>
2019-02-27 17:50:32 -0800

    Add an additional validation for:
    	1. File names doesn’t start with .
    	2. Verify the image
    	3. Corrupted images
    Made some correction to filename parser to remove the file extension before parsing

--- intropyproject-classify-pet-images/get_pet_labels.py
+++ intropyproject-classify-pet-images/get_pet_labels.py
@@ -5,7 +5,7 @@
 #
 # PROGRAMMER: Susanta pattanayak
 # DATE CREATED:  02/08/2019
-# REVISED DATE: 02/25/2019
+# REVISED DATE: 02/27/2019
 # PURPOSE: Create the function get_pet_labels that creates the pet labels from 
 #          the image's filename. This function inputs: 
 #           - The Image Folder as image_dir within get_pet_labels function and 
@@ -18,7 +18,8 @@
 #
 ##
 # Imports python modules
-from os import listdir
+from os import listdir, path
+from PIL import Image
 
 
 # TODO 2: Define get_pet_labels function below please be certain to replace None
@@ -42,10 +43,6 @@ def get_pet_labels(image_dir):
       List. The list contains for following item:
          index 0 = pet image label (string)
     """
-    # Replace None with the results_dic dictionary that you created with this
-    # function
-    # Imports only listdir function from OS module
-    # Retrieve the filenames from folder pet_images/
 
     # Get the filename from the image directory
     filename_list = listdir(image_dir)
@@ -53,18 +50,53 @@ def get_pet_labels(image_dir):
 
     # Iterate through the filename to generate the pet label
     for f_id in range(0, len(filename_list), 1):
-        if filename_list[f_id] not in results_dic:
-            pet_label = ""
 
-            #  format the pet labels so that they are in all lower case letters
-            #     and with leading and trailing whitespace characters stripped from them.
-            #     (ex. filename = 'Boston_terrier_02259.jpg' Pet label = 'boston terrier')
+        # validate the image file. and if Valid then continue or just ignore that file
 
-            for word in filename_list[f_id].lower().split("_"):
-                if word.isalpha():
-                    pet_label += word + ' '
+        if validate_image_file(image_dir, filename_list[f_id]):
+            file_name = path.splitext(filename_list[f_id])[0]
 
-            pet_label = pet_label.strip()
-            results_dic[filename_list[f_id]] = [pet_label]
+            if filename_list[f_id] not in results_dic:
+                pet_label = ""
+
+                #  format the pet labels so that they are in all lower case letters
+                #     and with leading and trailing whitespace characters stripped from them.
+                #     (ex. filename = 'Boston_terrier_02259.jpg' Pet label = 'boston terrier')
+
+                for word in file_name.lower().split("_"):
+                    if word.isalpha():
+                        pet_label += word + ' '
+
+                pet_label = pet_label.strip()
+                results_dic[filename_list[f_id]] = [pet_label]
 
     return results_dic
+
+
+def validate_image_file(image_dir, file_name):
+    """
+    Function to validate the image and print the Error results if any
+    :param image_dir: image directory input
+    :param file_name: image filename
+    :return: Returns if the image file is valid
+
+    """
+    # filename parser should detect and fully ignore any filename starting with '.'
+    # Notes: I could have used an if-else. But preferred 'continue'
+    is_valid = True
+    image_file_path = path.join(image_dir, file_name)
+    if file_name.startswith("."):
+        print("\n\nERROR: File {} is not a valid file name as it starts with  '.'".format(file_name))
+        is_valid = False
+        return is_valid
+    # Check if the file has the right extension
+    try:
+        file_image = Image.open(image_file_path)
+        file_image.verify()
+        return is_valid
+
+    except OSError as e:
+        # Print the invalid image details
+        print('Exception: {} ERROR: File {} is not a valid image \n'.format(e, file_name))
+        is_valid = False
+        return is_valid
--- intropyproject-classify-pet-images/print_results_hints.py
+++ intropyproject-classify-pet-images/print_results_hints.py
@@ -33,8 +33,8 @@
 #       Notice that this function doesn't return anything because it prints 
 #       a summary of the results using results_dic and results_stats_dic
 # 
-def print_results(results_dic, results_stats_dic, model, 
-                  print_incorrect_dogs = False, print_incorrect_breed = False):
+def print_results(results_dic, results_stats_dic, model,
+                  print_incorrect_dogs=False, print_incorrect_breed=False):
     """
     Prints summary results on the classification and then prints incorrectly 
     classified dogs and incorrectly classified dog breeds if user indicates 
@@ -63,9 +63,9 @@ def print_results(results_dic, results_stats_dic, model,
                               False doesn't print anything(default) (bool) 
     Returns:
            None - simply printing results.
-    """   
+    """
     # Prints summary statistics over the run
-    print("\n\n*** Results Summary for CNN Model Architecture",model.upper(), 
+    print("\n\n*** Results Summary for CNN Model Architecture", model.upper(),
           "***")
     print("{:20}: {:3d}".format('N Images', results_stats_dic['n_images']))
     print("{:20}: {:3d}".format('N Dog Images', results_stats_dic['n_dogs_img']))
@@ -76,7 +76,6 @@ def print_results(results_dic, results_stats_dic, model,
     #          results_stats_dic
     #
     print("")
-
 
     # Prints summary statistics (percentages) on Model Run
     print(" ")
@@ -90,21 +89,19 @@ def print_results(results_dic, results_stats_dic, model,
         #           both the key and the value. Remember the value is accessed 
         #           by results_stats_dic[key]
         #
-        pass 
+        pass
 
-
-    # IF print_incorrect_dogs == True AND there were images incorrectly 
+        # IF print_incorrect_dogs == True AND there were images incorrectly
     # classified as dogs or vice versa - print out these cases
-    if (print_incorrect_dogs and 
-        ( (results_stats_dic['n_correct_dogs'] + results_stats_dic['n_correct_notdogs'])
-          != results_stats_dic['n_images'] ) 
-       ):
+    if (print_incorrect_dogs and
+            ((results_stats_dic['n_correct_dogs'] + results_stats_dic['n_correct_notdogs'])
+             != results_stats_dic['n_images'])
+    ):
         print("\nINCORRECT Dog/NOT Dog Assignments:")
 
         # process through results dict, printing incorrectly classified dogs
         for key in results_dic:
-
-            # TODO: 6c. REPLACE pass with CODE that prints out the pet label 
+            # TODO: 6c. REPLACE pass with CODE that prints out the pet label
             #           and the classifier label from results_dic dictionary    
             #           ONLY when the classifier function (classifier label) 
             #           misclassified dogs specifically: 
@@ -126,16 +123,16 @@ def print_results(results_dic, results_stats_dic, model,
 
     # IF print_incorrect_breed == True AND there were dogs whose breeds 
     # were incorrectly classified - print out these cases                    
-    if (print_incorrect_breed and 
-        (results_stats_dic['n_correct_dogs'] != results_stats_dic['n_correct_breed']) 
-       ):
+    if (print_incorrect_breed and
+            (results_stats_dic['n_correct_dogs'] != results_stats_dic['n_correct_breed'])
+    ):
         print("\nINCORRECT Dog Breed Assignment:")
 
         # process through results dict, printing incorrectly classified breeds
         for key in results_dic:
 
             # Pet Image Label is-a-Dog, classified as-a-dog but is WRONG breed
-            if ( sum(results_dic[key][3:]) == 2 and
-                results_dic[key][2] == 0 ):
+            if (sum(results_dic[key][3:]) == 2 and
+                    results_dic[key][2] == 0):
                 print("Real: {:>26}   Classifier: {:>30}".format(results_dic[key][0],
-                                                          results_dic[key][1]))
+                                                                 results_dic[key][1]))
